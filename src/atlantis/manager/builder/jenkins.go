f9df0de76a262fd65b53c36f06af18ca35c16a09
Jigish Patel <jigish@ooyala.com>
2013-11-25 11:03:07 -0800

    make job name configurable

--- src/atlantis/manager/builder/jenkins.go
+++ src/atlantis/manager/builder/jenkins.go
@@ -9,21 +9,22 @@ import (
 
 type JenkinsBuilder struct {
 	URL string
+	Job string
 }
 
-func NewJenkinsBuilder(url string) *JenkinsBuilder {
-	return &JenkinsBuilder{URL: url}
+func NewJenkinsBuilder(url, job string) *JenkinsBuilder {
+	return &JenkinsBuilder{URL: url, Job: job}
 }
 
 func (b *JenkinsBuilder) Build(t *Task, repo, root, sha string) (io.ReadCloser, error) {
 	jenkins.JENKINS_SERVER = b.URL
 	t.LogStatus("Triggering Jenkins Build")
-	info, err := jenkins.DoBuild("atlantis-build", "app_repo="+repo+"&app_root="+root+"&app_commit="+sha, true)
+	info, err := jenkins.DoBuild(b.Job, "app_repo="+repo+"&app_root="+root+"&app_commit="+sha, true)
 	if err != nil {
 		return nil, errors.New("Jenkins Error: " + err.Error())
 	}
 	if info.Result != "SUCCESS" {
 		return nil, errors.New("Jenkins Build " + info.Url + " " + info.Result)
 	}
-	return jenkins.GetArtifactReader("atlantis-build", info.Id, ManifestFile)
+	return jenkins.GetArtifactReader(b.Job, info.Id, ManifestFile)
 }
