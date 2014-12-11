ffb6fc142c0bb9c922a439d2455f569cede1ed2a
Evan Danaher <edanaher@ooyala.com>
2014-12-10 16:43:26 -0800

    Client: Migrate app.go to genericExecutor

--- src/atlantis/manager/client/app.go
+++ src/atlantis/manager/client/app.go
@@ -21,28 +21,12 @@ type RequestAppDependencyCommand struct {
 	App        string   `short:"a" long:"app" description:"the app to request a dependency for"`
 	Dependency string   `short:"d" long:"dependency" description:"the dependency to request"`
 	Envs       []string `short:"e" long:"env" description:"the envs to request the dependency in"`
+	Arg        ManagerRequestAppDependencyArg
+	Reply      ManagerRequestAppDependencyReply
 }
 
 func (c *RequestAppDependencyCommand) Execute(args []string) error {
-	err := Init()
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("Request App Dependency...")
-	args = ExtractArgs([]*string{&c.App, &c.Dependency}, args)
-	arg := ManagerRequestAppDependencyArg{
-		ManagerAuthArg: dummyAuthArg,
-		App:            c.App,
-		Dependency:     c.Dependency,
-		Envs:           c.Envs,
-	}
-	var reply ManagerRequestAppDependencyReply
-	err = rpcClient.CallAuthed("RequestAppDependency", &arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("-> Status: %s", reply.Status)
-	return Output(map[string]interface{}{"status": reply.Status}, nil, nil)
+	return genericExecuter(c, args)
 }
 
 // ----------------------------------------------------------------------------------------------------------
@@ -52,80 +36,35 @@ func (c *RequestAppDependencyCommand) Execute(args []string) error {
 type AddDependerAppDataCommand struct {
 	App      string `short:"a" long:"app" description:"the app to add a depender for"`
 	FromFile string `short:"f" long:"file" description:"the file to pull the data from"`
+	Arg      ManagerRequestAppDependencyArg
+	Reply    ManagerRequestAppDependencyReply
 }
 
 func (c *AddDependerAppDataCommand) Execute(args []string) error {
-	err := Init()
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("Add Depender App...")
-	args = ExtractArgs([]*string{&c.App, &c.FromFile}, args)
-	data := &DependerAppData{}
-	file, err := os.Open(c.FromFile)
-	if err != nil {
-		return OutputError(err)
-	}
-	jsonDec := json.NewDecoder(file)
-	if err := jsonDec.Decode(data); err != nil {
-		return OutputError(err)
-	}
-  arg := ManagerAddDependerAppDataArg{ManagerAuthArg: dummyAuthArg, App: c.App, DependerAppData: data}
-	var reply ManagerAddDependerAppDataReply
-	err = rpcClient.CallAuthed("AddDependerAppData", &arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("-> Status: %s", reply.Status)
-	LogApp(reply.App)
-	return Output(map[string]interface{}{"status": reply.Status, "app": reply.App}, nil, nil)
+	return genericExecuter(c, args)
 }
 
 type RemoveDependerAppDataCommand struct {
 	App      string `short:"a" long:"app" description:"the app to remove a depender from"`
 	Depender string `short:"r" long:"depender" description:"the depender app to remove"`
+	Arg      ManagerRemoveDependerAppDataArg
+	Reply    ManagerRemoveDependerAppDataReply
 }
 
 func (c *RemoveDependerAppDataCommand) Execute(args []string) error {
-	err := Init()
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("Remove Depender App...")
-	args = ExtractArgs([]*string{&c.App, &c.Depender}, args)
-	arg := ManagerRemoveDependerAppDataArg{ManagerAuthArg: dummyAuthArg, App: c.App, Depender: c.Depender}
-	var reply ManagerRemoveDependerAppDataReply
-	err = rpcClient.CallAuthed("RemoveDependerAppData", &arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("-> Status: %s", reply.Status)
-	LogApp(reply.App)
-	return Output(map[string]interface{}{"status": reply.Status, "app": reply.App}, nil, nil)
+	return genericExecuter(c, args)
 }
 
 type GetDependerAppDataCommand struct {
-	App      string `short:"a" long:"app" description:"the app to get a depender from"`
-	Depender string `short:"r" long:"depender" description:"the depender app to get"`
+	App        string `short:"a" long:"app" description:"the app to get a depender from"`
+	Depender   string `short:"r" long:"depender" description:"the depender app to get"`
+	Properties string `message:"Get Depender App" field:"DependerAppData" name:"DependerAppData"`
+	Arg        ManagerGetDependerAppDataArg
+	Reply      ManagerGetDependerAppDataReply
 }
 
 func (c *GetDependerAppDataCommand) Execute(args []string) error {
-	err := Init()
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("Get Depender App...")
-	args = ExtractArgs([]*string{&c.App, &c.Depender}, args)
-	arg := ManagerGetDependerAppDataArg{ManagerAuthArg: dummyAuthArg, App: c.App, Depender: c.Depender}
-	var reply ManagerGetDependerAppDataReply
-	err = rpcClient.CallAuthed("GetDependerAppData", &arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("-> Status: %s", reply.Status)
-	Log("-> DependerAppData:")
-	LogDependerAppData("  ", reply.DependerAppData)
-	return Output(map[string]interface{}{"status": reply.Status, "dependerAppData": reply.DependerAppData}, nil, nil)
+	return genericExecuter(c, args)
 }
 
 // ----------------------------------------------------------------------------------------------------------
@@ -165,50 +104,26 @@ func (c *AddDependerEnvDataCommand) Execute(args []string) error {
 }
 
 type RemoveDependerEnvDataCommand struct {
-	App string `short:"a" long:"app" description:"the app to remove an env from"`
-	Env string `short:"e" long:"env" description:"the env to remove"`
+	App        string `short:"a" long:"app" description:"the app to remove an env from"`
+	Env        string `short:"e" long:"env" description:"the env to remove"`
+	Properties string `message:"Remove Depender Env" field:"App" name:"app"`
+	Arg        ManagerRemoveDependerEnvDataArg
+	Reply      ManagerRemoveDependerEnvDataReply
 }
 
 func (c *RemoveDependerEnvDataCommand) Execute(args []string) error {
-	err := Init()
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("Remove Depender Env...")
-	args = ExtractArgs([]*string{&c.App, &c.Env}, args)
-	arg := ManagerRemoveDependerEnvDataArg{ManagerAuthArg: dummyAuthArg, Env: c.Env, App: c.App}
-	var reply ManagerRemoveDependerEnvDataReply
-	err = rpcClient.CallAuthed("RemoveDependerEnvData", &arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("-> Status: %s", reply.Status)
-	LogApp(reply.App)
-	return Output(map[string]interface{}{"status": reply.Status, "app": reply.App}, nil, nil)
+	return genericExecuter(c, args)
 }
 
 type GetDependerEnvDataCommand struct {
-	App string `short:"a" long:"app" description:"the app to get an env from"`
-	Env string `short:"e" long:"depender" description:"the env to get"`
+	App   string `short:"a" long:"app" description:"the app to get an env from"`
+	Env   string `short:"e" long:"depender" description:"the env to get"`
+	Arg   ManagerGetDependerEnvDataArg
+	Reply ManagerGetDependerEnvDataReply
 }
 
 func (c *GetDependerEnvDataCommand) Execute(args []string) error {
-	err := Init()
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("Get Depender Env...")
-	args = ExtractArgs([]*string{&c.App, &c.Env}, args)
-	arg := ManagerGetDependerEnvDataArg{ManagerAuthArg: dummyAuthArg, Env: c.Env, App: c.App}
-	var reply ManagerGetDependerEnvDataReply
-	err = rpcClient.CallAuthed("GetDependerEnvData", &arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("-> Status: %s", reply.Status)
-	Log("-> DependerEnvData:")
-	LogDependerEnvData("  ", reply.DependerEnvData)
-	return Output(map[string]interface{}{"status": reply.Status, "dependerEnvData": reply.DependerEnvData}, nil, nil)
+	return genericExecuter(c, args)
 }
 
 // ----------------------------------------------------------------------------------------------------------
@@ -257,57 +172,23 @@ type RemoveDependerEnvDataForDependerAppCommand struct {
 	App      string `short:"a" long:"app" description:"the app to remove an env from"`
 	Depender string `short:"r" long:"depender" description:"the depender to add an env for"`
 	Env      string `short:"e" long:"env" description:"the env to remove"`
+	Arg      ManagerRemoveDependerEnvDataForDependerAppArg
+	Reply    ManagerRemoveDependerEnvDataForDependerAppReply
 }
 
 func (c *RemoveDependerEnvDataForDependerAppCommand) Execute(args []string) error {
-	err := Init()
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("Remove Depender Env For Depender App...")
-	args = ExtractArgs([]*string{&c.App, &c.Env}, args)
-	arg := ManagerRemoveDependerEnvDataForDependerAppArg{
-		ManagerAuthArg: dummyAuthArg,
-		Env:            c.Env,
-		Depender:       c.Depender,
-		App:            c.App,
-	}
-	var reply ManagerRemoveDependerEnvDataForDependerAppReply
-	err = rpcClient.CallAuthed("RemoveDependerEnvDataForDependerApp", &arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("-> Status: %s", reply.Status)
-	LogApp(reply.App)
-	return Output(map[string]interface{}{"status": reply.Status, "app": reply.App}, nil, nil)
+	return genericExecuter(c, args)
 }
 
 type GetDependerEnvDataForDependerAppCommand struct {
-	App      string `short:"a" long:"app" description:"the app to get an env from"`
-	Depender string `short:"r" long:"depender" description:"the depender to add an env for"`
-	Env      string `short:"e" long:"env" description:"the env to get"`
+	App        string `short:"a" long:"app" description:"the app to get an env from"`
+	Depender   string `short:"r" long:"depender" description:"the depender to add an env for"`
+	Env        string `short:"e" long:"env" description:"the env to get"`
+	Properties string `field:"DependerEnvData"`
+	Arg        ManagerGetDependerEnvDataForDependerAppArg
+	Reply      ManagerGetDependerEnvDataForDependerAppReply
 }
 
 func (c *GetDependerEnvDataForDependerAppCommand) Execute(args []string) error {
-	err := Init()
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("Get Depender Env For Depender App...")
-	args = ExtractArgs([]*string{&c.App, &c.Env}, args)
-	arg := ManagerGetDependerEnvDataForDependerAppArg{
-		ManagerAuthArg: dummyAuthArg,
-		Env:            c.Env,
-		Depender:       c.Depender,
-		App:            c.App,
-	}
-	var reply ManagerGetDependerEnvDataForDependerAppReply
-	err = rpcClient.CallAuthed("GetDependerEnvDataForDependerApp", &arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("-> Status: %s", reply.Status)
-	Log("-> DependerEnvData:")
-	LogDependerEnvData("  ", reply.DependerEnvData)
-	return Output(map[string]interface{}{"status": reply.Status, "dependerEnvData": reply.DependerEnvData}, nil, nil)
+	return genericExecuter(c, args)
 }
