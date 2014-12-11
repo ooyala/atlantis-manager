ace3518b6f03c364041aaeda85bf76cf58a05526
Evan Danaher <edanaher@ooyala.com>
2014-12-10 16:45:52 -0800

    Client: Migrate maintenance.go to genericExecutor

--- src/atlantis/manager/client/maintenance.go
+++ src/atlantis/manager/client/maintenance.go
@@ -16,39 +16,22 @@ import (
 )
 
 type ContainerMaintenanceCommand struct {
-	Container   string `short:"c" long:"container" description:"the container to set maintenance for"`
+	ContainerID string `short:"c" long:"container" description:"the container to set maintenance for"`
 	Maintenance bool   `short:"m" long:"maintenance" description:"true to set maintenance mode"`
+	Arg         ManagerContainerMaintenanceArg
+	Reply       ManagerContainerMaintenanceReply
 }
 
 func (c *ContainerMaintenanceCommand) Execute(args []string) error {
-	if err := Init(); err != nil {
-		return OutputError(err)
-	}
-	args = ExtractArgs([]*string{&c.Container}, args)
-	Log("ContainerMaintenance ...")
-	arg := ManagerContainerMaintenanceArg{ManagerAuthArg: dummyAuthArg,
-		ContainerID: c.Container, Maintenance: c.Maintenance}
-	var reply ManagerContainerMaintenanceReply
-  err := rpcClient.CallAuthed("ContainerMaintenance", &arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("ContainerMaintenance %s.", reply.Status)
-	return Output(map[string]interface{}{"status": reply.Status}, nil, nil)
+	return genericExecuter(c, args)
 }
 
 type IdleCommand struct {
+	Properties string `noauth:"true"`
+	Arg        ManagerIdleArg
+	Reply      ManagerIdleReply
 }
 
 func (c *IdleCommand) Execute(args []string) error {
-	InitNoLogin()
-	Log("Idle ...")
-	arg := ManagerIdleArg{}
-	var reply ManagerIdleReply
-	err := rpcClient.Call("Idle", arg, &reply)
-	if err != nil {
-		return OutputError(err)
-	}
-	Log("Idle %t.", reply.Idle)
-	return Output(map[string]interface{}{"status": reply.Status, "idle": reply.Idle}, reply.Idle, nil)
+	return genericExecuter(c, args)
 }
